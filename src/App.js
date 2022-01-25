2a169fbea2039fbb4ad8b7e665de979414fe712c
super1-1star <agsuperstar1142@gmail.com>
2022-01-26 04:22:24 +0530

    final update

--- src/App.js
+++ src/App.js
@@ -5,23 +5,47 @@ import Expenses from './components/Expenses/Expenses';
 
 const DUMMY_EXPENSES = [
   {
-    id: 'e1',
-    title: 'Toilet Paper',
+    id: "e1",
+    title: "Toilet Paper",
     amount: 94.12,
-    date: new Date(2020, 7, 14),
+    date: new Date(2021, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
   {
-    id: 'e3',
-    title: 'Car Insurance',
+    id: "e2",
+    title: "Pen",
+    amount: 94.12,
+    date: new Date(2021, 8, 14),
+  },
+  {
+    id: "e3",
+    title: "Pencil",
+    amount: 94.12,
+    date: new Date(2021, 7, 14),
+  },
+  {
+    id: "e4",
+    title: "cookies",
+    amount: 94.12,
+    date: new Date(2020, 3, 14),
+  },
+  {
+    id: "e5",
+    title: "Cricket Bat",
+    amount: 94.12,
+    date: new Date(2020, 10, 14),
+  },
+  { id: "e6", title: "New TV", amount: 799.49, date: new Date(2022, 2, 12) },
+  {
+    id: "e7",
+    title: "Car Insurance",
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2019, 2, 28),
   },
   {
-    id: 'e4',
-    title: 'New Desk (Wooden)',
+    id: "e8",
+    title: "New Desk (Wooden)",
     amount: 450,
-    date: new Date(2021, 5, 12),
+    date: new Date(2018, 5, 12),
   },
 ];
 
@@ -34,12 +58,6 @@ const App = () => {
     });
   };
 
-  // return React.createElement(
-  //   'div',
-  //   {},
-  //   React.createElement('h2', {}, "Let's get started!"),
-  //   React.createElement(Expenses, { items: expenses })
-  // );
 
   return (
     <div>
